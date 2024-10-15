e1e84ca7b95fccd6566477d4a9f19064cda5c44d
hvckless <hvckless@gmail.com>
2024-10-15 16:33:03 +0900

    [Bug] Test

--- src/com/humanmedi/miniproject/GlobalDefine.java
+++ src/com/humanmedi/miniproject/GlobalDefine.java
@@ -1,7 +1,9 @@
 package com.humanmedi.miniproject;
 
+import com.humanmedi.miniproject.entity.Player;
 import com.humanmedi.miniproject.stage.Stage;
 
 public class GlobalDefine {
     static Stage current_stage = null;
+    static Player player = null;
 }
