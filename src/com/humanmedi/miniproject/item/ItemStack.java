ca660ab6ffec3eb2e4d50c5eceb6e8b76e65bf2f
hvckless <hvckless@gmail.com>
2024-10-15 14:58:42 +0900

    [Feat] 플레이어 / 인벤토리 / 아이템 / 아이템 - 무기 구현

--- src/com/humanmedi/miniproject/item/ItemStack.java
+++ src/com/humanmedi/miniproject/item/ItemStack.java
@@ -5,32 +5,15 @@ public class ItemStack {
 
     private Material material = null;
 
-    public String name;
-
-
-
-
 
     public ItemStack(Material material){
         this.material = material;
     }
 
 
-    
-
-
-
-
-
-
-    public String getName(){
-        return this.name;
+    public void setMaterial(Material material) {
+    	this.material = material;
     }
-    
-    public int getDamage(){
-        return 10;
-    }
-
     public Material getMaterial(){
         return this.material;
     }
