b58d0b848cb58506b97fbec9e6e980077de8ceed
tlagothf <sunsol33@naver.com>
2024-10-15 16:10:54 +0900

    랜덤한 속성 반환

---
+++ src/com/humanmedi/miniproject/stage/MonsterSpawner.java
@@ -0,0 +1,12 @@
+package com.humanmedi.miniproject.stage;
+import java.util.List;
+import java.util.Random;
+
+public interface MonsterSpawner {
+    default Monster getRandomMonster(List<Monster> monsters){
+        Random random = new Random();
+        return monsters.get(random.nextInt(monsters.size()));
+    }
+
+    void spawnMonster();
+}
